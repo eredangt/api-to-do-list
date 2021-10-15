ff0ca83a2af3a2fdb98c0fc4c8880c8e919c1f98
eredangt <caio.lopes@estudante.ufla.br>
2021-10-15 17:25:47 -0300

    Tarefas-Model.js completed

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose")
 
 const estudante_controller = require('./estudantes-controller')
 
-mongoose.connect('mongodb+srv://usuario:senha@cluster0.nkc2c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://toDoList:J530wleRuf2Vlhfs@cluster-api-to-do-list.kcurz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 mongoose.Promise = global.Promise
 try {
     let db = mongoose.connection
-    db.on('errr', console.error.bind(console, 'erro de conexao no banco'))
+    db.on('errr', console.error.bind(console, 'Erro de Conexão no Banco'))
 } catch (e) {
     console.log(e)
 }
---
+++ tarefas-model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+let TarefasSchema = new Schema({
+    id: { type: Number, required: true },
+    description: { type: String, required: false },
+    deadline: { type: Date, required: true },
+    complete: { type: Boolean, required: true }
+})
+
+module.exports = mongoose.model('Tarefas', TarefasSchema)
